6091973c97f7fae94736c9fe992c553536e97d11
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-05-14 12:50:27 +0300

    Configuration management using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,6 +7,13 @@ package { 'python3-pip':
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/flask --version | grep "2.1.0"',
+  unless  => '/usr/bin/flask --version 2>&1 | grep "Flask 2.1.0"',
+  require => Package['python3-pip'],
+}
+
+file { '/usr/local/bin/flask':
+  ensure => link,
+  target => '/usr/bin/flask',
+  require => Exec['install_flask'],
 }
 
